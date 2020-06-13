acafeeb48d83b5e0f555eb475241d34204015e2b
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2020-06-13 15:48:03 +0800

    Updated and Added new fun commands

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.on('ready', () => {
         maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
         interval: 2000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting banned. 
         warningMessage: "Please stop spamming", // Message users receive when warned. (message starts with '@User, ' so you only need to input continue of it.) 
-        banMessage: "has been hit by ban hammer for spamming!", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.) 
+        banMessage: "has been banned for spamming!", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.) 
         maxDuplicatesWarning: 7, // Maximum amount of duplicate messages a user can send in a timespan before getting warned.
         maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned.
         deleteMessagesAfterBanForPastDays: 7, // Deletes the message history of the banned user in x days.
@@ -45,6 +45,24 @@ bot.on('message', async message => {
 
     if (message.author.bot) return;
     if (!message.content.startsWith(PREFIX)) return;
+
+    // User XP
+
+    // if the user is not on db add the user and change his values to 0
+    if (!db[message.author.id]) db[message.author.id] = {
+        xp: 0,
+        level: 0
+      };
+    db[message.author.id].xp++;
+    let userInfo = db[message.author.id];
+    if(userInfo.xp > 100) {
+        userInfo.level++
+        userInfo.xp = 0
+        message.reply("Congratulations, you levelled up")
+    }
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
     // Other Commands
     switch (args[0]) {
 
@@ -60,7 +78,7 @@ bot.on('message', async message => {
             var botIcon = message.guild.iconURL
                 .setTitle('Modapro™')
                 .setThumbnail(botIcon)
-                .addField('Bot Name', 'Modapro')
+                .addField('Bot Name', 'Moderation Bot')
                 .addField('Developers:', 'Main commands developed by CrypticDevv, Music commands developed by Existance')
                 .addField('How do I see all the commands?', 'Do .cmds')
                 .addField('What do I do?', 'My purpose is to replace all bots in the Discord!')
@@ -129,7 +147,7 @@ bot.on('message', async message => {
             message.channel.send('Invite Link: https://discordapp.com/oauth2/authorize?client_id=581704684780257300&scope=bot');
             break;
         case ('poll'):
-
+            message.channel.search('Currently a Work In Progress.')
             break;
             // Moderation Code
         case ('kick'):
@@ -314,11 +332,65 @@ bot.on('message', async message => {
                 await process.exit()
                 console.log('Bot Terminated by', message.author.username)
                 break;
-            case (''):
-
+            case ('raidshadowlegends'):
+                message.channel.send("Today's message is sponsored by Raid Shadow Legends, one of the biggest mobile role-playing games of 2019 and it's totally free! Currently almost 10 million users have joined Raid over the last six months, and it's one of the most impressive games in its class with detailed models, environments and smooth 60 frames per second animations! All the champions in the game can be customized with unique gear that changes your strategic buffs and abilities! The dungeon bosses have some ridiculous skills of their own and figuring out the perfect party and strategy to overtake them's a lot of fun! Currently with over 300,000 reviews, Raid has almost a perfect score on the Play Store! The community is growing fast and the highly anticipated new faction wars feature is now live, you might even find my squad out there in the arena! It's easier to start now than ever with rates program for new players you get a new daily login reward for the first 90 days that you play in the game! So what are you waiting for? Go to the video description, click on the special links and you'll get 50,000 silver and a free epic champion as part of the new player program to start your journey! Good luck and I'll see you there!")
                 break;
+            case ('dice'):
+                var num = 0
+                num = Math.floor((Math.random() * 6) + 1)
+                switch (num) {
+                    case 1:
+                        message.channel.send('1')
+                        break;
+                    case 2:
+                        message.channel.send('2')
+                        break;
+                    case 3:
+                        message.channel.send('3')
+                        break;
+                    case 4:
+                        message.channel.send('4')
+                        break;
+                    case 5:
+                        message.channel.send('5')
+                        break;
+                    case 6:
+                        message.channel.send('6')
+                        break;
+                }
+                break;
+            case ('rickroll'):
+                message.channel.send('Currently a work in progress.')
+                break;
+                
+            case ('userxpstatus'):
+                message.channel.send('User XP status is currently in development')
+                break;
+
+            case ('userxp'):
+                let userInfo = db[message.author.id];
+                let member = message.mentions.members.first();
+                let embed = new Discord.RichEmbed()
+                .setColor(0x4286f4)
+                .addField("Level", userInfo.level)
+                .addField("XP", userInfo.xp+"/100");
+                if(!member) return message.channel.sendEmbed(embed)
+                let memberInfo = db[member.id]
+                let embed2 = new Discord.RichEmbed()
+                .setColor(0x4286f4)
+                .addField("Level", memberInfo.level)
+                .addField("XP", memberInfo.xp+"/100")
+                message.channel.send(embed2)
+                break;
+            case ('math'):
+                break;
+
     }
     client.emit('checkMessage', message);
+
+    fs.writeFile("./database.json", JSON.stringify(db), (x) => {
+        if (x) console.error(x)
+      });
 })
 
 
